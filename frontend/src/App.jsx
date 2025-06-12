c500dbd41af38f992a2174a470551a2d1f91264d
ShifraBsyr12 <sifrabasayar12@gmail.com>
2025-06-12 13:41:39 +0530

    Review some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/home';
 import DriverDashboard from './Pages/DriverDashboard';
+import Review from './Pages/Review';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/DriverDashboard" element={<DriverDashboard />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/DriverDashboard" element={<DriverDashboard />} />
+        <Route path="/review" element={<Review />} /> {}
+      </Routes>
+    </Router>
   );
 }
 
